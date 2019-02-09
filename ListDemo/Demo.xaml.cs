76725270e91ae7e4676cf4184a501e2d3b70457d
Dustin Walker <thewalkman1234@gmail.com>
2019-02-09 14:32:05 -0600

    ConextActions

---
+++ ListDemo/Demo.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using ListDemo.Models;
+using Xamarin.Forms;
+
+namespace ListDemo
+{
+    public partial class Demo : ContentPage
+    {
+        void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            var contact = e.Item as Contact;
+            DisplayAlert("Selected", contact.Name, "OK");
+        }
+
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            var contact = e.SelectedItem as Contact;
+            DisplayAlert("Tapped", contact.Name, "OK");
+        }
+
+        public Demo()
+        {
+            InitializeComponent();
+
+
+
+            listView.ItemsSource = new List<ContactGroup>{
+
+                new ContactGroup("D", "D") {
+                    new Contact { Name = "Dustin", ImageUrl = "http://lorempixel.com/100/100/people/1" }
+                },
+
+                new ContactGroup("B", "B") {
+                    new Contact { Name = "Brad", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Blue lives Matte!" }
+                }
+
+            };
+
+        }
+    }
+}
+
---
+++ ListDemo/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListDemo.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
